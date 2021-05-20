74cc9399dd80ef48637748540a032ea815df7a60
林嘉軒 <mike200177@hotmail.com>
2021-05-21 00:34:23 +0800

    測試輸出文字串接及印出速度

--- Source.asm
+++ Source.asm
@@ -5,6 +5,8 @@ consoleInfo	CONSOLE_SCREEN_BUFFER_INFO <>
 consoleRowSize DWORD ?
 consoleColumnSize DWORD ?
 
+output BYTE 10000 DUP(?), 0
+
 .code
 main PROC
 
@@ -20,14 +22,44 @@ invoke GetConsoleScreenBufferInfo, eax, addr consoleInfo
 movzx eax, consoleInfo.srWindow.Bottom
 movzx ebx, consoleInfo.srWindow.Top
 sub eax, ebx
+inc eax
 mov consoleRowSize, eax
 
 movzx eax, consoleInfo.srWindow.Right
 movzx ebx, consoleInfo.srWindow.Left
 sub eax, ebx
 mov consoleColumnSize, eax
-; -----------------------------------------------------------------------------
 
+; Test Output
+mov dl, 42
+mov esi, 0
+mov ecx, consoleRowSize
+L1 :
+	push ecx
+	mov ecx, consoleColumnSize
+	L2 :
+		mov output[esi], dl
+		inc esi
+	loop L2
+	mov output[esi], 10
+	pop ecx
+	inc esi
+loop L1
+
+mov edx, OFFSET output
+call WriteString
+call Crlf
+
+;push esi
+;push ecx
+;mov esi, OFFSET output
+;mov ebx, TYPE output
+;mov ecx, LENGTHOF output
+;call DumpMem
+;pop ecx
+;pop esi
+
+; -----------------------------------------------------------------------------
 
 
 exit
